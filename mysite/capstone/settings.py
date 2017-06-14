93b35c71d04ef7447f83dfe94b2b352d56e5788c
Dean Smith <Dean Smith>
2017-06-14 00:40:19 -0500

    Added readme. And adding django-material

--- mysite/capstone/settings.py
+++ mysite/capstone/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material.theme.lightblue',
+    'material',
+    'material.admin',
     'reminder.apps.ReminderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
