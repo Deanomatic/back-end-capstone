dce551b9b2377a26d4b0d255715abc302857d691
Deanomatic <jonthandsmith19@gmail.com>
2017-06-16 12:23:05 -0500

    Added base and home templates.

--- mysite/reminders/urls.py
+++ mysite/reminders/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
+ 
 from .views import ReminderListView, ReminderCreateView, ReminderDetailView, ReminderUpdateView, ReminderDeleteView
 
-
+ 
 
 urlpatterns = [
     # List and detail views
